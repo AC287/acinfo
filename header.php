3b84c1d2aa8412a94257f507f83d487e342f687a
AC <arthurchen287@gmail.com>
2019-04-06 23:46:06 -0400

    Setting up base.

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+  <head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- <title><?php //bloginfo('name');?></title> -->
+    <?php wp_head(); ?>
+    <?php //include 'phpsnippet/serverlocation.php';?>
+    <?php //include 'phpsnippet/titletag.php';?>
+
+  </head>
+
+  <body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<div class="container">
+  <p>HELLLOO</p>
+</div>
+<?php get_footer(); ?>
